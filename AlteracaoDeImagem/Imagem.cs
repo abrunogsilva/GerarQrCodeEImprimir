94d9de507da4d0a62d211f4fc14266599f601546
Bruno Guerreiro <brunimfic@gmail.com>
2023-10-26 08:55:12 -0300

    Inclusão da solução

---
+++ AlteracaoDeImagem/Imagem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlteracaoDeImagem
+{
+    public class Imagem
+    {
+        //public void alterarImagem(Image bitmapDeQrcode, string textodescricao)
+        //{
+        //    Graphics graphics = Graphics.FromImage(bitmapDeQrcode);
+
+        //    // Define text color
+        //    Brush brush = new SolidBrush(Color.Black);
+
+        //    // Define text font
+        //    Font arial = new Font("Arial", 30, FontStyle.Regular);
+
+        //    //Texto descrição
+        //    string text = textodescricao;
+
+        //    // Define rectangle
+        //    Rectangle rectangle = new Rectangle(400, 1500, 1600, 150);
+
+        //    // Specify rectangle border
+        //    Pen pen = new Pen(Color.White, 2);
+
+        //    // Draw rectangle
+        //    graphics.DrawRectangle(pen, rectangle);
+
+        //    // Draw text on image
+        //    graphics.DrawString(text, arial, brush, rectangle);
+        //}
+        
+    }
+}
---
+++ ConsoleRegistroTlb/Program.cs
@@ -0,0 +1,15 @@
+﻿using GerarQrCode;
+
+namespace ConsoleRegistroTlb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var patch = @"C:\\Users\\bruno.silva\\AppData\\Local\\Temp\\8025EC13-3BC2-4A3E-8512-D1465467.jpg";
+            QrCode ok = new QrCode();
+            ok.GerarQrcode(300, 300 , "Teste de Geracao teste de geracao de geração macavi",patch, "Teste de Geracao teste de geracao de geração macavi", 12);
+            ok.ImprimirImagem(patch, "Microsoft Print to PDF");
+        }
+    }
+}
